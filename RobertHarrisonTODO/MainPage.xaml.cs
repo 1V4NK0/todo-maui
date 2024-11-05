10e4eef176ac2876452cb3e1d517dbc3efb04eb0
1v4nk0 <1vancherk3s@gmail.com>
2024-11-05 18:08:03 +0000

    second commit

--- RobertHarrisonTODO/MainPage.xaml.cs
+++ RobertHarrisonTODO/MainPage.xaml.cs
@@ -1,25 +1,33 @@
-﻿namespace RobertHarrisonTODO;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 
+namespace RobertHarrisonTODO;
+//dotnet build -t:Run -f net8.0-ios
+//dotnet build -t:Run -f net8.0-maccatalyst 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public ObservableCollection<Task> Tasks = new ObservableCollection<Task>();
 
 	public MainPage()
 	{
 		InitializeComponent();
+		Title = "";
+        
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    async void AddTaskBtn_Clicked(System.Object sender, System.EventArgs e)
+    {
+        string description = await DisplayPromptAsync("Task description", "Enter your task description");
+        if (description == null)
+        {
+            await DisplayAlert("Error","You have to enter description to create a task", "OK");
+            return;
+        }
+        Task newTask = new Task(description, false);
+        Tasks.Add(newTask);
+    }
 }
 
 
---
+++ RobertHarrisonTODO/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace RobertHarrisonTODO
+{
+	public class Task
+	{
+		string description { get; set; }
+		bool completed { get; set; }
+
+		public Task(string description, bool completed)
+		{
+			this.description = description;
+			this.completed = completed;
+		}
+	}
+}
+
